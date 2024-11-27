6b4413aaa613bcf8225672d339577cdb8e9bd107
Shankar Bhandari <harryshankar@outlook.com>
2024-11-27 14:01:23 +0000

    import packages &&  data

---
+++ Main.R
@@ -0,0 +1,7 @@
+# List of required libraries
+required_packages <- c("tidyverse", "GGally", "ggfortify", "cluster","ggpubr")
+# Load all libraries
+lapply(required_packages, library, character.only = TRUE)
+# importing data
+data = read_csv('data/index_of_economic_freedom_2024.csv')
+print(data)
